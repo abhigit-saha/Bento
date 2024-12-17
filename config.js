72b6332f08e3d5aac393da3bd3865bc28bf143ed
Abhijit Saha <63587168+abhigit-saha@users.noreply.github.com>
2024-12-17 14:51:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Codeforces',
+			icon: 'code',
+			link: 'https://codeforces.com',
 		},
 		{
 			id: '6',
